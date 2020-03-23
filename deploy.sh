31be64c82ec3bb874591f8feca8b9b728b097e94
snow treetree <18817834076@163.com>
2020-03-23 17:47:20 +0800

    feat: 测试travis ci

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 echo "test 1"
+cd build
+
+scp -o stricthostkeychecking=no -P 22 -r ./* latiao@106.14.156.134:/www/buzhi
--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Row, Col,Button } from 'antd';
+
+
+
+Function.prototype.bindCus = function bindCus(context) {
+  const self = this;
+  const arg1 = Array.prototype.slice(arguments,1)
+  return function(){
+    return self.apply(context,arg1.concat(Array.prototype.slice(arguments,1)))
+  }
+}
+
+function testBind(name,age) {
+  console.log(name,age)
+  console.log(this.name)
+}
+
+const obj = {name:'zxl'}
+
+const fun = testBind.bind(obj,'name')
+
+fun(18)
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header id="header">
+      <Row>
+      <Col span={6}>
+      <h1 className="align-center" span={6}>buZhi&</h1>
+      </Col>
+      <Col span={18}>
+        </Col>
+      </Row>
       </header>
+      <div id="content"></div>
+      <footer>
+        <div className="align-center">
+        © 2020-2020 buzhifangxue.com 版权所有<Button target="_blank" type="link" href="http://www.beian.miit.gov.cn/">蜀ICP备20006050号</Button>
+        </div></footer>
     </div>
   );
 }
